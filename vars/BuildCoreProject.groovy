68e05cfb449dd79be2973f8a49cccdda9d45414e
nasurfriend <73640940+nasurfriend@users.noreply.github.com>
2022-04-01 17:31:59 +0800

    Update BuildCoreProject.groovy

--- vars/BuildCoreProject.groovy
+++ vars/BuildCoreProject.groovy
@@ -23,23 +23,7 @@ def call(Map config) {
 		  
 		  stage("Pull from repository"){
 			  steps{
-						withCredentials([usernamePassword(credentialsId: "nexus", passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-							statusCode=sh(script: "curl --write-out '%{http_code}' -s -o /dev/null -u $Username:$Password -v -X GET '${REPO_URL}'", returnStdout: true).trim().tokenize("\n")
-							echo "HTTP response status code: $statusCode"
-						}
-						if(statusCode==['200']) {
-								withCredentials([usernamePassword(credentialsId: "nexus", passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-									sh "mkdir ${WORKDIR} && cd ${WORKDIR} && curl --write-out '%{http_code}' -s -L -u $USERNAME:$PASSWORD -v -X GET '${REPO_URL}' --output ${FILE_NAME}"
-								}
-								
-							withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-								unzip dir: "${WORKDIR}/${BUILD_FOLDER}", glob: "", zipFile: "${WORKDIR}/${FILE_NAME}"
-							}
-							deployKey(ec2_cred,ec2_ip)
-						} else {
-							currentBuild.result = 'ABORTED'
-							error('Apps Not Found')
-						}
+				echo "Pulling from repository:"
 			  }
 		  }
 		  

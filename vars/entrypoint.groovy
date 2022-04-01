b49abe147c31d6a559edaeb8fd02d93740c67729
nasurfriend <73640940+nasurfriend@users.noreply.github.com>
2022-04-01 17:00:46 +0800

    Update entrypoint.groovy

--- vars/entrypoint.groovy
+++ vars/entrypoint.groovy
@@ -1,1 +1,37 @@
-TEST="basically this means nothing without a nothing"
+#!/usr/bin/env groovy
+
+def call() {
+    echo " here are the stages of the pipelines that need to be exsecuted"
+  pipeline {
+    agent any
+	
+    stages {
+      
+		  stage("Clean Workspace") {
+        rm -rf *
+      }
+      
+      stage("Pull from repository"){
+          withCredentials([usernamePassword(credentialsId: "nexus", passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+						statusCode=sh(script: "curl --write-out '%{http_code}' -s -o /dev/null -u $Username:$Password -v -X GET '${REPO_URL}'", returnStdout: true).trim().tokenize("\n")
+						echo "HTTP response status code: $statusCode"
+					}
+					if(statusCode==['200']) {
+					        withCredentials([usernamePassword(credentialsId: "nexus", passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+					            sh "mkdir ${WORKDIR} && cd ${WORKDIR} && curl --write-out '%{http_code}' -s -L -u $USERNAME:$PASSWORD -v -X GET '${REPO_URL}' --output ${FILE_NAME}"
+					        }
+							
+						withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+							unzip dir: "${WORKDIR}/${BUILD_FOLDER}", glob: "", zipFile: "${WORKDIR}/${FILE_NAME}"
+						}
+						deployKey(ec2_cred,ec2_ip)
+					} else {
+						currentBuild.result = 'ABORTED'
+						error('Apps Not Found')
+					}
+      }
+      
+    }
+    
+  }
+}
